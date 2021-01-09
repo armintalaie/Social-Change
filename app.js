dabf3b390c359de8a76b1409fa53fb1f3da7899a
armintalaie <talaiearmin78@gmail.com>
2021-01-09 15:35:48 -0800

    added sign in, sign up, sign out authentication to backend

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const path = require("path");
+const userRoute = require('./routes/userRoute')
 
 const app = express();
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port);
