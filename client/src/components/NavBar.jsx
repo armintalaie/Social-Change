55b49debedd023b0f8a45518d3977434f977b7c1
Alina Chalanuchpong <alina.c@alumni.ubc.ca>
2021-01-09 20:58:21 -0500

    add sass

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from "reactstrap";
+
+function NavBar({ props }) {
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <Nav className="mr-auto" navbar>
+          <NavbarBrand href="/">MOMENTUM</NavbarBrand>
+          <NavItem>
+            <NavLink href="/community/">Communities</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/movement/">Movements</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/ambassador/">Ambassadors</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavBar;
