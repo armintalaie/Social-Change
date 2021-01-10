e9bb7200e1aca780459c360bf4fc4f8f2f7d9158
Kevin Huang <kevinh42@student.ubc.ca>
2021-01-10 05:50:48 -0800

    Using new api

--- routes/movement.js
+++ routes/movement.js
@@ -1,42 +1,23 @@
 const express = require("express");
 const router = express.Router()
-const User = require('../models/user')
 const Movement = require('../models/movement')
-const Community = require('../models/community')
-var MongoClient = require('mongodb').MongoClient
-const mongoose = require('mongoose')
-const fs = require('fs')
-const db = require('../db')
+require('../db.js')
+const db = require('../database.js')
 
-const movement_vote_pts = 1
-const num_top_amsdrs = 5
-const num_top_mvments = 8
-const mvmnt_donation_pts = 4
-const cmnty_donation_pts = 10
+router.post('/create', async (req, res) => {
 
-
-
-router.post('/create', (req, res) => {
-
-    var mv = Movement()
-
+    let mv = Movement()
 
     mv.name = req.body.name
     mv.description = req.body.description
     mv.goal = req.body.goal
+    mv.count = 0;
+    mv.passed = false;
+    mv.created_by = req.user._id;
 
-    mv.votes.push(req.user._id)
-
-    db.add_movement(req.user._id, mv._id)
-
-    mv.save()
-        .then((result) => {
-            console.log('sssss')
-            res.render('index')
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+    await db.createMovement(req.user._id, mv)
+    
+    res.render('index')
 })
 
 
