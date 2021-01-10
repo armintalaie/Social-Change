9ef552a762689859e41c9d0aaaf84d9596f78fe3
Alina Chalanuchpong <alina.c@alumni.ubc.ca>
2021-01-09 19:52:18 -0500

    install bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { Alert } from "reactstrap";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <Alert color="primary">This is a primary alert — check it out!</Alert>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
