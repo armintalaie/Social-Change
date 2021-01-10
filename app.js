9d7f57b73de6085b40a90edbf94908709501a040
Aymen Dirar <63937200+adirar111@users.noreply.github.com>
2021-01-10 08:28:58 -0800

    can attach communities to movements

--- app.js
+++ app.js
@@ -1,75 +1,63 @@
 const express = require("express");
 const path = require("path");
-const fs = require('fs')
-const mongoose = require('mongoose')
-const MongoClient = require('mongodb').MongoClient;
+const fs = require("fs");
+const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
 
 const db = require("./database");
-var bodyParser = require('body-parser')
-const session = require('express-session')
+var bodyParser = require("body-parser");
+const session = require("express-session");
 
-const movements = require('./routes/movement')
-const community = require('./routes/community')
-const passport = require('passport')
-require("./passport")(passport)
-const userRoute = require('./routes/userRoute')
+const movements = require("./routes/movement");
+const community = require("./routes/community");
+const passport = require("passport");
+require("./passport")(passport);
+const userRoute = require("./routes/userRoute");
 
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static(path.join(__dirname, "client/build")));
 
-app.set('views', __dirname + '/public/views')
-app.set('view engine', 'ejs')
+app.set("views", __dirname + "/public/views");
+app.set("view engine", "ejs");
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: false }))
+app.get("/", (req, res) => {
+    res.redirect("/home");
+});
 
+app.get("/home", async (req, res) => {
+    res.locals.movements = await db.getAllMovements();
+    res.render("lp");
+});
 
+app.use(
+    session({
+        secret: "secret",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
 
-app.get('/', (req, res) => {
+app.use(passport.initialize());
+app.use(passport.session());
 
-
-    res.redirect('/home')
-
-})
-
-app.get('/home', async(req, res) => {
-
-    res.locals.movements = await db.getAllMovements()
-    res.render('lp')
-
-})
-
-
-
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true,
-}))
-
-app.use(passport.initialize())
-app.use(passport.session())
-
-
-
-app.use(movements)
-app.use(userRoute)
-app.use(community)
+app.use(movements);
+app.use(userRoute);
+app.use(community);
 
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-
-
 async function test() {
-
-    let user = await db.getUser(mongoose.Types.ObjectId("5ffa380bc6c7e25d3d20af53"));
+    let user = await db.getUser(
+        mongoose.Types.ObjectId("5ffa380bc6c7e25d3d20af53")
+    );
     console.log(user);
 }
 test();
-
 
 console.log(`Password generator listening on ${port}`);
--- routes/movement.js
+++ routes/movement.js
@@ -1,32 +1,39 @@
 const express = require("express");
-const router = express.Router()
-const Movement = require('../models/movement')
-require('../db.js')
-const db = require('../database.js')
+const router = express.Router();
+const Movement = require("../models/movement");
+require("../db.js");
+const db = require("../database.js");
+const mongoose = require("mongoose");
 
-router.post('/create', async (req, res) => {
+router.post("/create", async (req, res) => {
+    let mv = Movement();
 
-    let mv = Movement()
-
-    mv.name = req.body.name
-    mv.description = req.body.description
-    mv.goal = req.body.goal
+    mv.name = req.body.name;
+    mv.description = req.body.description;
+    mv.goal = req.body.goal;
     mv.count = 0;
     mv.passed = false;
     mv.created_by = req.user._id;
 
-    await db.createMovement(req.user._id, mv)
-    
-    res.render('index')
-})
+    let community = req.body.community;
+    console.log(req.body.goal);
+    console.log(community);
+    console.log(typeof community);
+    mv.community = mongoose.Types.ObjectId(community);
 
+    // mv.communities = community._id;
 
-router.get('/create', (req, res) => {
-    if (!req.user)
-        res.redirect('/signin')
+    await db.createMovement(req.user._id, mv);
 
-    res.render('createMovement')
-})
+    res.render("index");
+});
 
+router.get("/create", async (req, res) => {
+    if (!req.user) res.redirect("/signin");
 
-module.exports = router
+    res.locals.communities = await db.getAllCommunities();
+    console.log(res.locals.communities);
+    res.render("createMovement");
+});
+
+module.exports = router;
