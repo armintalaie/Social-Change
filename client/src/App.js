83f755f959f47e10baa017a0f22b6d39d0253345
Aymen Dirar <63937200+adirar111@users.noreply.github.com>
2021-01-09 18:32:25 -0800

    navbar + setup for signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import { Component } from "react";
 import { Alert } from "reactstrap";
 
+// component imports
+import Navbar from "./components/navbar";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          <Alert color="primary">This is a primary alert — check it out!</Alert>
+    return (
+        <div className="App">
+            <div>
+                <Navbar />
+                <Alert color="primary">
+                    This is a primary alert — check it out!
+                </Alert>
+            </div>
         </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ client/src/components/navbar.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText,
+    Button,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+
+export default class NavbarLoggedIn extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            // <div>
+            //     <Navbar color="light" light expand="md">
+            //         <NavbarBrand>LOGO</NavbarBrand>
+            //         <NavbarToggler />
+            //         <Collapse navbar>
+            //             <Nav className="mr-auto" navbar>
+            //                 <NavItem>Dashboard</NavItem>
+            //                 <NavItem>Communities</NavItem>
+            //                 <NavItem>Movements</NavItem>
+            //                 <NavItem>Ambassadors</NavItem>
+            //             </Nav>
+            //             <NavbarText>Simple Text</NavbarText>
+            //         </Collapse>
+            //     </Navbar>
+            // </div>
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand>
+                        <img
+                            src="https://lh3.googleusercontent.com/proxy/uGSQ0xNV8RCJ5rNkba5lb-TLSF94P3pQq2JS-athV9cvKuqnuFCEE0sVl-SKd-PzaG6ZqOgdeP7rgqEb0fMEOvKIdU4qHU0NbQCgjbwJ7zVbjeQu"
+                            width="50px"
+                        />
+                    </NavbarBrand>
+                    <NavbarToggler />
+                    <Collapse navbar>
+                        <Nav className="mr-auto" navbar>
+                            <NavItem>
+                                <NavLink href="#">Dashboard</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">Communities</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">Movements</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">Ambassadors</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                    <Button color="primary" className="m-1">
+                        Sign In
+                    </Button>
+                    <Button color="primary" className="m-1">
+                        Sign Up
+                    </Button>
+                </Navbar>
+            </div>
+        );
+    }
+}
