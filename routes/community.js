602f288af53e14f72a19a542eacee1a374181ce5
Kevin Huang <kevinh42@student.ubc.ca>
2021-01-10 08:28:58 -0800

    Added movement cards to community page

--- routes/community.js
+++ routes/community.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
 const User = require('../models/user')
 const Community = require('../models/community')
 const Photo = require('../models/photo')
